84b46ecbc23e6b3ff67e4f6c5473143b01380613
Gerson Calvo <gedacaba@gmail.com>
2023-04-27 16:20:22 -0500

    Usando el operador Where

--- Program.cs
+++ Program.cs
@@ -1,12 +1,26 @@
 ﻿LinqQueries queries = new LinqQueries();
 
-ImprimirValores(queries.TodaLaColeccion());
+//muestra toda la coleccion
+//ImprimirValores(queries.TodaLaColeccion());
 
+
+//libros despues del 2000
+//ImprimirValores(queries.LibrosDespuesdel2000());
+
+
+///Libros que tienen mas de 250 pags y tienen en el titulo la palabra in action
+ImprimirValores(queries.LibrosConMasde250PagConPalabrasInAction());
+
+//toda la coleccion
 void ImprimirValores(IEnumerable<Book> listadelibros)
 {
     Console.WriteLine("{0,-60} {1, 15} {2, 15}\n", "Titulo", "N. Paginas", "Fecha publicacion");
+   
     foreach(var item in listadelibros)
     {
-        Console.WriteLine("{0,-60} {1, 15} {2, 15}", item.Title, item.PageCount, item.PublishedDate.ToShortDateString());
+        //Console.WriteLine("{0,-60} {1, 15} {2, 15}", item.Title, item.PageCount, item.PublishedDate.ToShortDateString());
+
+        Console.WriteLine("{0,-60} {1, 15} {2, 15}", item.Title, item.PageCount, item.PublishedDate);
+
     }
 }
